8cd978f3dc6912f304ffc7e106f8a0aaf4b1b11f
Nikos Fotopoulos <nick.foto.ret@gmail.com>
2024-06-15 14:36:08 +0200

    proposed setup of sources

--- proj2.m
+++ proj2.m
@@ -4,24 +4,33 @@ clear; close; clc;
 NUM_SOURCES = 5;
 NUM_MICROPHONES = 4;
 
-% Load impulse respones and audio file of interest
+% Load impulse respones and audio files (same fs)
 H = struct2cell(load('impulse_responses.mat'));
-[y, fs] = audioread('datasets/clean_speech.wav');
+[s1, ~] = audioread('datasets/clean_speech_2.wav');
+[s2, ~] = audioread('datasets/babble_noise.wav');
+[s3, ~] = audioread('datasets/Speech_shaped_noise.wav');
+[s4, ~] = audioread('datasets/aritificial_nonstat_noise.wav');
+[s5, fs] = audioread('datasets/clean_speech.wav');
+
+% Truncate all files to the same length
+min_length = min([length(s1), length(s2), length(s3), ...
+                  length(s4), length(s5)]);
+S = cat(2, s1(1:min_length), s2(1:min_length), ...
+    s3(1:min_length), s4(1:min_length), s5(1:min_length));
 
 % Convolve the impulse responses for each source - microphone pair
-signal_length = size(y, 1);
+signal_length = min_length;
 signals_sources_mics = zeros(signal_length, 5, 4);
 for i = 1:NUM_SOURCES
     for j = 1:NUM_MICROPHONES
-        signals_sources_mics(:, i, j) = conv(y, H{i}(j,:), "same");
+        signals_sources_mics(:, i, j) = conv(S(:,i), H{i}(j,:), "same");
     end
 end
 
-% Superposition from each source each microphone:
+% Superposition from each source at each microphone:
 signals_mics = squeeze(sum(signals_sources_mics, 2));
 
 % Now we're trying to isolate source 5 (target)...
-% Perform delay and sum beamforming
 
 
 
