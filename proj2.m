1ac9cf595c8e12938cd8e1fddfd8d868746eec45
Nikos Fotopoulos <nick.foto.ret@gmail.com>
2024-06-15 18:28:28 +0200

    update eqns for cross psd matrices

--- proj2.m
+++ proj2.m
@@ -12,13 +12,17 @@ H = struct2cell(load('impulse_responses.mat'));
 [s4, ~] = audioread('datasets/aritificial_nonstat_noise.wav');
 [s5, fs] = audioread('datasets/clean_speech.wav');  % Target source
 
-% Pad the shorter signals with zeros
-max_length = max([length(s1), length(s2), length(s3), length(s4), length(s5)]);
+% Pad the shorter signals with zeros and scale clean signal
+scaling_factor = 2;
+% lengths = [length(s1), length(s2), length(s3), length(s4), length(s5)];
+% disp(lengths);
+% max_length = max(lengths);
+max_length = length(s5);
 s1 = [s1; zeros(max_length - length(s1), 1)];
-s2 = [s2; zeros(max_length - length(s2), 1)];
-s3 = [s3; zeros(max_length - length(s3), 1)];
-s4 = [s4; zeros(max_length - length(s4), 1)];
-s5 = [s5; zeros(max_length - length(s5), 1)];
+s2 = s2(1:max_length);
+s3 = s3(1:max_length);
+s4 = s4(1:max_length);
+s5 = scaling_factor*s5;
 S = cat(2, s1, s2, s3, s4, s5);
 
 % Convolve the impulse responses for each source - microphone pair
@@ -32,6 +36,24 @@ end
 
 % Superposition from all sources at each microphone:
 signals_mics = squeeze(sum(signals_sources_mics, 2));
+
+% Perform STFT
+N = 320;   % 20ms or alternatively N=256 for 16ms
+[X,F,T] = stft(signals_mics, fs, Window=hamming(N), OverlapLength=N/2, FFTLength=N);
+
+% Empirical cross PSD
+alpha = 0.5;
+K = size(F,1);
+L = size(T,1);
+Rx = zeros(K, L, NUM_MICROPHONES, NUM_MICROPHONES);
+for k = 1:K
+    vec_x = X(k,1,:);
+    Rx(k,1,:,:) = vec_x(:)*vec_x(:)';
+    for l = 2:L
+        vec_x = X(k,l,:);
+        Rx(k,l,:,:) = alpha*squeeze(Rx(k,l-1,:,:)) + (1-alpha)* vec_x(:)*vec_x(:)';
+    end
+end
 
 % Now we're trying to isolate source 5 (target)...
 
