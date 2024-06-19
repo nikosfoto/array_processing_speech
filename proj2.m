7a3b96d256fca789c01d3691b02986f3c528582b
Nikos Fotopoulos <nick.foto.ret@gmail.com>
2024-06-19 17:02:53 +0200

    updated SNR and improved ease of parameter sweep

--- proj2.m
+++ proj2.m
@@ -1,4 +1,4 @@
-close; clc;
+close;
 
 % Define problem variables
 NUM_SOURCES = 5;
@@ -48,7 +48,7 @@ corrupted = corrupted * (max_clean/max_corrupted);
 snrOriginal = 20*log10( norm(clean) / norm(corrupted-clean) );
 
 % Display the original SNR
-fprintf('Original SNR between clean speech and corrupted signal: %.2fdB\n\n', snrOriginal);
+fprintf('Original SNR between clean speech and corrupted signal: %.2fdB\n', snrOriginal);
 
 % Calculate STOI for the original corrupted signal
 stoiOriginal = stoi(clean, corrupted, fs);
@@ -67,74 +67,74 @@ noisy_frames = findnoise(s5, fs, N/2);
 % Finding the empirical cross PSD, and filter for each frequency bin and
 % time frame
 
-% Parameters to be set
-alpha = 0.5;
-mu = 0.7; 
-K = size(F,1);
-L = size(T,1);
-Rx = zeros(NUM_MICROPHONES, NUM_MICROPHONES);
-Rn = zeros(NUM_MICROPHONES, NUM_MICROPHONES);
-S = zeros(K, L);
+% Parameters to be set (can sweep multiple alphas and mus)
+for alpha = 0.7
+    for mu = 0.7
+        
+        fprintf('alpha: %.1f, mu: %.1f\n', alpha, mu);
 
-for k = 1:K
-    for l = 1:L
-        if l==1
-            vec_x = X(k,1,:);
-            Rx = vec_x(:)*vec_x(:)';
-            Rn = vec_x(:)*vec_x(:)';
-        else
-            vec_x = X(k,l,:);
-            Rx = alpha*Rx_prev + (1-alpha)* vec_x(:)*vec_x(:)';
-            if noisy_frames(l) == 1
-                Rn = alpha*Rn_prev + (1-alpha)* vec_x(:)*vec_x(:)';
-            else
-                Rn = Rn_prev;
+        K = size(F,1);
+        L = size(T,1);
+        Rx = zeros(NUM_MICROPHONES, NUM_MICROPHONES);
+        Rn = zeros(NUM_MICROPHONES, NUM_MICROPHONES);
+        S = zeros(K, L);
+        
+        for k = 1:K
+            for l = 1:L
+                if l==1
+                    vec_x = X(k,1,:);
+                    Rx = vec_x(:)*vec_x(:)';
+                    Rn = vec_x(:)*vec_x(:)';
+                else
+                    vec_x = X(k,l,:);
+                    Rx = alpha*Rx_prev + (1-alpha)* vec_x(:)*vec_x(:)';
+                    if noisy_frames(l) == 1
+                        Rn = alpha*Rn_prev + (1-alpha)* vec_x(:)*vec_x(:)';
+                    else
+                        Rn = Rn_prev;
+                    end
+                end
+                [a,sigma_s] = estimate_a(Rx, Rn);
+                % Select one of the following filters
+        
+                % % MVDR
+                %inv_R = pinv(Rx);
+                %w = (inv_R *a ) / (a'*inv_R * a);
+        
+                % % Multi channel
+                % inv_R = pinv(Rn);
+                % w = (sigma_s*inv_R*a)/(sigma_s*(a'*inv_R*a)+1);
+        
+                % Signal distortion weighted
+                e = [1;0;0;0];
+                Rs = sigma_s * (a * a'); 
+                w = pinv(Rs + mu*Rn)* Rs*e;
+        
+                S(k,l) = w'*vec_x(:); 
+                Rn_prev = Rn;
+                Rx_prev = Rx;
             end
         end
-        [a,sigma_s] = estimate_a(Rx, Rn);
-        % Select one of the following filters
+        
+        % Performing inverse STFT
+        S(isnan(S))=0;
+        [s, t] = istft(S, fs, Window=hamming(N), OverlapLength=N/2, FFTLength=512);
+        
+        % Obtain resulting signal (real part)
+        processed_signal = real(s(end-clean_length+1:end));
+        max_processed = max(processed_signal);
+        processed_signal = processed_signal * (max_clean/max_processed);
+        
+        % Save audio file for later listening
+        audiowrite(sprintf('saved/alpha_%.1f_mu_%.1f.wav', alpha, mu), processed_signal, fs);
+        
+        % Calculate STOI
+        stoiValue = stoi(clean, processed_signal, fs);
+        fprintf('STOI between clean speech and processed signal: %.4f\n', stoiValue);
+        
+        % Finding the SNR
+        snrValue = 20*log10( norm(clean) / norm(clean - processed_signal) );
+        fprintf('SNR between clean speech and processed signal: %.2fdB\n\n', snrValue);
 
-        % % MVDR
-        %inv_R = pinv(Rx);
-        %w = (inv_R *a ) / (a'*inv_R * a);
-
-        % % Multi channel
-        % inv_R = pinv(Rn);
-        % w = (sigma_s*inv_R*a)/(sigma_s*(a'*inv_R*a)+1);
-
-        % Signal distortion weighted
-        e = [1;0;0;0];
-        Rs = sigma_s * (a * a'); 
-        w = pinv(Rs + mu*Rn)* Rs*e;
-
-        S(k,l) = w'*vec_x(:); 
-        Rn_prev = Rn;
-        Rx_prev = Rx;
     end
 end
-
-% Performing inverse STFT
-S(isnan(S))=0;
-[s, t] = istft(S, fs, Window=hamming(N), OverlapLength=N/2, FFTLength=512);
-
-% Obtain resulting signal (real part)
-processed_signal = real(s(end-clean_length+1:end));
-max_processed = max(processed_signal);
-processed_signal = processed_signal * (max_clean/max_processed);
-
-%%
-% PLotting the clean and recovered signals
-hold("on")
-plot(s5)
-plot(real(s))
-hold("off")
-
-%%
-% Calculate STOI
-stoiValue = stoi(clean, processed_signal, fs);
-fprintf('STOI between clean speech and processed signal: %.4f\n', stoiValue);
-
-%%
-% Finding the SNR
-snrValue = 20*log10( norm(clean) / norm(clean - processed_signal) );
-fprintf('SNR between clean speech and processed signal: %.2fdB\n\n', snrValue);
